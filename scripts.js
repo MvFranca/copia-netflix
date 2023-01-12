71eb92f381051e9756bccc19f7184d77ec1d9746
M4RC0S-V1N1C1US <mvfs4@aluno.ifal.edu.br>
2023-01-12 19:01:43 -0300

    adicionei o menu de login

---
+++ scripts.js
@@ -0,0 +1,16 @@
+
+function menulogin(){
+   let menu = document.getElementById('registro')
+
+   if(menu.style.display  == 'none'){
+    menu.style.display = 'block'
+   } else{
+    menu.style.display = 'none'
+   }
+    
+}
+
+function sumirmenu(){
+    let menu = document.getElementById('registro')
+    menu.style.display = 'none'
+}
